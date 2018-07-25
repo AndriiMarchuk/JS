79f236770961c7cd7cff1d37e07d2b1fb4b56830
user <user@gmail.com>
2018-07-25 12:25:13 +0300

    Classes

--- luxoft-web007-js/m10prototype/Trade.js
+++ luxoft-web007-js/m10prototype/Trade.js
@@ -7,16 +7,17 @@ Stock.prototype.totalPrice = function () {
     return this.price * this.count;
 }
 Stock.prototype.toString = function () {
-    return "Stock => company: " + this.company + "; count: " + this.count + "; totalPrice: " + Stock.prototype.totalPrice.call(this);
+    return "Stock => company: " + this.company + "; count: " + this.count + "; totalPrice: " + this.totalPrice();
 }
 
 var stock = new Stock("Luxoft", 50, 8);
 console.log(stock.toString());
 
 function ForwardStock(company, price, forwardPrice, count) {
-    Stock.call(this, company, price, count);
+    Stock.call(ForwardStock.prototype, company, price, count);
     this.forwardPrice = forwardPrice;
 }
+ForwardStock.prototype = Object.create(Stock.prototype);
 ForwardStock.prototype.toString = function () {
     return Stock.prototype.toString.call(this) + "; forwardPrice: " + this.forwardPrice;
 }
